9f083d54db9ee8048c48f7bd68096eb7e82a6a06
Igordrs <igordrs@outlook.com>
2024-06-23 11:05:14 -0300

    first commit

---
+++ HistoricosApp.API/Extensions/MessagesConfigExtension.cs
@@ -0,0 +1,36 @@
+﻿using HistoricosApp.API.Consumers;
+using HistoricosApp.API.Settings;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace HistoricosApp.API.Extensions
+{
+    public static class MessagesConfigExtension
+    {
+        public static IServiceCollection AddMessagesConfig
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitMQSettings = new RabbitMQSettings();
+            new ConfigureFromConfigurationOptions<RabbitMQSettings>
+                (configuration.GetSection("RabbitMQ")).Configure(rabbitMQSettings);
+
+            services.AddSingleton(rabbitMQSettings);
+
+            services.AddSingleton<IConnectionFactory>(cf =>
+            {
+                var config = cf.GetRequiredService<RabbitMQSettings>();
+                return new ConnectionFactory
+                {
+                    HostName = config.Hostname,
+                    Port = config.Port,
+                    UserName = config.Username,
+                    Password = config.Password,
+                };
+            });
+
+            services.AddHostedService<MessageConsumer>();
+
+            return services;
+        }
+    }
+}
